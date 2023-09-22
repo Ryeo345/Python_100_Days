91e185008b6834a44038b8c8f61f573b53128367
Ryeo345 <philipruan345@gmail.com>
2023-09-22 17:28:35 -0400

    finished day 9 exercises

--- Python_Notes/Python Day 9 - Dictionaries.py
+++ Python_Notes/Python Day 9 - Dictionaries.py
@@ -83,3 +83,33 @@ travel_log = {
 }
 
 # can have multiple dictionaries in a list
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+#🚨 Do NOT change the code above
+
+# #TODO: Write the function that will allow new countries
+# #to be added to the travel_log. 👇
+#
+# def add_new_country(country, visits, cities_visited):
+#     new_log = {}
+#
+#     new_log["country"] = country
+#     new_log["visits"] = visits
+#     new_log["cities"] = cities_visited
+#
+#     travel_log.append(new_log)
+#
+#
+# #🚨 Do not change the code below
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log)
