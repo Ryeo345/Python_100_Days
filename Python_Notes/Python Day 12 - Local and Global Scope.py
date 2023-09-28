4662852f25e5066a64a101f849628a3903878658
Ryeo345 <philipruan345@gmail.com>
2023-09-27 21:17:16 -0400

    added guessing game

--- Python_Notes/Python Day 12 - Local and Global Scope.py
+++ Python_Notes/Python Day 12 - Local and Global Scope.py
@@ -12,4 +12,20 @@ python does not have block scope
 variable defined with a value in an if statement will effect the variable in a function,
 
 if the code is not enclosed, the variable will have global scope
+
+if the variable is in a function, and we want to modify the variable, that is already defined in the global scope
+
+we need
+
+"global" keyword variable
+
+which is fallible and prone to causing errors
+
+its better to return the modification in the function and assign the new value to the variable outside of local scope
+
+- Python constants are variables that we don't ever want to change i.e. pi
+
+the naming convetion is all uppercase
+
+PI = 3.14159
 """
